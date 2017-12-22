b8b825f95069e7c231a6abf48057c517cd2cba9a
Mahesh Velankar <mvelanka@gmail.com>
2017-12-22 18:04:12 +0000

    corrected frequency and other details for speaker

--- data/mr/speakerprop.sh
+++ data/mr/speakerprop.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+DOL1=$1
+echo "got arg 1 = " ${DOL1}
 
 export SPEAKER="$1";
 export DATASET="man_mh";
@@ -7,12 +9,12 @@ export VER=1;
 export QNAME="mr_IN.talk";
 export QNUM=001;
 export IDIOMA="mr";
-export SAMPFREQ=16000;
-export FRAMESHIFT=80;
-export FRAMELEN=400;
-export FREQWARP=0.42;
+export SAMPFREQ=48000;
+export FRAMESHIFT=240;
+export FRAMELEN=1200;
+export FREQWARP=0.55;
 export NORMALIZE=1; # with 0 I get out of range errors
-export FFTLEN=512;
+export FFTLEN=2048;
 
 if test "$1" = "slt" -o \
         "$1" = "clb"; then
@@ -26,8 +28,8 @@ elif test "$1" = "bdl" -o \
         "$1" = "rms" -o \
         "$1" = "ksp"; then
    export GENDER="male";
-   export LOWERF0="40";
-   export UPPERF0="280";
+   export LOWERF0="77";
+   export UPPERF0="227";
 fi
 
 if test "$1" = "awb"; then
@@ -43,4 +45,10 @@ elif test "$1" = "bdl" -o \
         "$1" = "clb"; then
    export DIALECT="american";
 fi
+   export LOWERF0="77";
+   export UPPERF0="227";
+echo "arg 1 set = " $1
+echo "speaker set = " ${SPEAKER}
+echo "lower freq set = " ${LOWERF0}
+echo "upper freq set = " ${UPPERF0}
 
